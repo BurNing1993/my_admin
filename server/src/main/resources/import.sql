6de1667aa0b36ca9fe485b697d9bb5f301346784
Joey <786678257@qq.com>
2020-11-02 10:58:22 +0800

    chore:imort.sql

--- server/src/main/resources/import.sql
+++ server/src/main/resources/import.sql
@@ -1,6 +1,7 @@
-INSERT INTO  `user`(`id`, `create_time`, `update_time`, `created_by`, `updated_by`, `cellphone`, `email`, `has_deleted`, `nickname`, `password`, `username`) VALUES (1, NULL, NULL, NULL, NULL, '19912345678', '123@qq.com', 0, 'Joey', 'd83b6339-7f85-4521-b50f-db8fb7ae0d46', 'admin');
+INSERT INTO `my_admin`.`sys_user`(`id`, `create_time`, `update_time`, `created_by`, `updated_by`, `cellphone`, `email`, `enabled`, `has_deleted`, `nickname`, `password`, `username`) VALUES (1, '2020-10-19 03:33:24.183363', '2020-10-19 03:33:24.211321', 'anonymousUser', 'anonymousUser', NULL, NULL, 1, 0, 'joey1234', '$2a$10$A0qc2on9ZwZmvzTVf3IDSOwe2BnQ7y6RuT6R27qfS8UxknO0IOL0i', 'joey1234');
 
-INSERT INTO  `role`(`id`, `create_time`, `update_time`, `created_by`, `updated_by`, `has_deleted`, `role_name`) VALUES (1, NULL, NULL, NULL, NULL, 0, 'ROLE_USER');
-INSERT INTO  `role`(`id`, `create_time`, `update_time`, `created_by`, `updated_by`, `has_deleted`, `role_name`) VALUES (2, NULL, NULL, NULL, NULL, 0, 'ROLE_ADMIN');
+INSERT INTO `my_admin`.`sys_role`(`id`, `create_time`, `update_time`, `created_by`, `updated_by`, `has_deleted`, `role_name`, `role_desc`) VALUES (1, NULL, NULL, NULL, NULL, 0, 'ADMIN', '管理员');
+INSERT INTO `my_admin`.`sys_role`(`id`, `create_time`, `update_time`, `created_by`, `updated_by`, `has_deleted`, `role_name`, `role_desc`) VALUES (2, NULL, NULL, NULL, NULL, 0, 'USER', '普通用户');
 
-INSERT INTO  `user_role`(`id`, `create_time`, `update_time`, `created_by`, `updated_by`, `role_id`, `user_id`) VALUES (1, NULL, NULL, NULL, NULL, 1, 1);
+
+INSERT INTO `my_admin`.`sys_user_role`(`user_id`, `role_id`) VALUES (1, 1);
